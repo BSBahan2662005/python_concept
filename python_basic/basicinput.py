56cad62bcd08ed78247d89942de8ac7e8e65155c
BSBahan2662005 <bahan6603@gmail.com>
2026-03-13 00:11:14 +0530

    first python committ

---
+++ python_basic/basicinput.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator.")
+city=input("Which city did you grow up in?\n")
+print(city)
+pet=input("What is the name of your pet?\n")
+print(pet)
+print("Your band name could be:"+  city + "" + pet )
